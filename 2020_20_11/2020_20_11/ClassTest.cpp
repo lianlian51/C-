13a2fc29d291c7074c74ee797e25cebedf0d09a1
lianlian51 <3282125330@qq.com>
2020-10-13 20:36:28 +0800

    更新代码

--- 2020_20_11/2020_20_11/ClassTest.cpp
+++ 2020_20_11/2020_20_11/ClassTest.cpp
@@ -1,3 +1,5 @@
+
+// 构造函数
 //#include <iostream>
 //
 //using namespace std;
@@ -131,7 +133,7 @@ int main()
 	Date d;
 	return 0;
 }
-
+// 析构函数
 #include <assert.h>
 #include <malloc.h>
 typedef int DataType;
@@ -163,4 +165,119 @@ private:
 	int* _pData;
 	size_t _size;
 	size_t _capacity;
+};
+
+// 拷贝构造函数
+class Date
+{
+public:
+	Date(int year = 1900, int month = 1, int day = 1)
+	{
+		_year = year;
+		_month = month;
+		_day = day;
+	}
+
+	Date(const Date& d)
+	{
+		_year = d._year;
+		_month = d._month;
+		_day = d._day;
+	}
+
+private:
+	int _year;
+	int _month;
+	int _day;
+};
+
+int main()
+{
+	Date d1;
+	Date d2(d1);
+	return 0;
+}
+
+class String {
+public:
+	String(const char* str = "jack")
+	{
+		_str = (char*)malloc(strlen(str) + 1);
+		strcpy(_str, str);
+	}
+
+	~String()
+	{
+		cout << "~String()" << endl;
+		free(_str);
+	}
+private:
+	char* _str;
+};
+
+int main()
+{
+	String s1("hello");
+	String s2(s1);
+}
+
+class Date
+{
+public:
+	Date(int year = 1900, int month = 1, int day = 1)
+	{
+		_year = year;
+		_month = month;
+		_day = day;
+	}
+
+	bool operator==(const Date& d2)
+	{
+		return (_year == d2._year && _month == d2._month && _day == d2._day);
+	}
+private:
+	int _year;
+	int _month;
+	int _day;
+};
+
+void Test()
+{
+	Date d1(2018, 9, 26);
+	Date d2(2018, 9, 27);
+	cout << (d1 == d2) << endl;
+}
+
+
+
+class Date
+{
+public:
+	Date(int year = 1900, int month = 1, int day = 1)
+	{
+		_year = year;
+		_month = month;
+		_day = day;
+	}
+
+	Date(const Date& d)
+	{
+		_year = d._year;
+		_month = d._month;
+		_day = d._day;
+	}
+	
+	Date& operator=(const Date& d)
+	{
+		if (this != &d)
+		{
+			_year = d._year;
+			_month = d._month;
+			_day = d._day;
+		}
+	}
+private:
+	int _year;
+	int _month;
+	int _day;
 };
