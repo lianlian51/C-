0782e8d9a50ac8c6d9f78a8f893fd2fd1ea2146e
lianlian51 <3282125330@qq.com>
2021-03-09 22:35:36 +0800

    十大经典算法

--- 春招思维导图/十大经典算法/十大经典算法/test.cpp
+++ 春招思维导图/十大经典算法/十大经典算法/test.cpp
@@ -159,6 +159,7 @@ void Adjust(vector<int> & nums, int len, int index)
 	}
 }
 
+// 堆排序
 void HeapSort(vector<int>& nums)
 {
 	int len = nums.size();
@@ -174,6 +175,110 @@ void HeapSort(vector<int>& nums)
 		swap(nums[0], nums[i]);
 		// 调整违反堆性质的区域
 		Adjust(nums, i, 0);
+	}
+}
+
+// 计数排序
+void countSort(int* nums, int size)
+{
+	// 1.找到最大的值
+	int max = nums[0];
+	for (int i = 0; i < size; ++i)
+	{
+		if (nums[i] > max)
+			max = nums[i];
+	}
+
+	// 2.确定数组的长度并进行初始化
+	int* countData = new int[max + 1];
+	for (int i = 0; i <= max; ++i)
+	{
+		countData[i] = 0;
+	}
+
+	// 3.遍历数组，统计每个数字的次数
+	for (int i = 0; i < size; ++i)
+		++countData[nums[i]];
+
+	// 4.累计输出数字
+	int index = 0;
+	for (int i = 0; i <= max; ++i)
+	{
+		for (int j = 0; j < countData[i]; ++j)
+		{
+			nums[index++] = i;
+		}
+	}
+}
+
+// 桶排序
+void bucketSort(int nums[], int size)
+{
+	int i, j;
+	int buckets[105];
+
+	for (i = 0; i < 105; ++i)
+	{
+		buckets[i] = 0;
+	}
+	for (i = 0; i < size; ++i)
+	{
+		++buckets[nums[i]];
+	}
+
+	for (i = 0, j = 0; i < 105; ++i)
+	{
+		while (buckets[i] > 0)
+		{
+			nums[j++] = i;
+			--buckets[i];
+		}
+	}
+}
+
+// 基数排序
+int maxbit(int data[], int n)
+{
+	int d = 1; //保存最大的位数
+	int p = 10;
+	for (int i = 0; i < n; ++i)
+	{
+		while (data[i] >= p)
+		{
+			p *= 10;
+			++d;
+		}
+	}
+	return d;
+}
+
+void radixsort(int data[], int n) //基数排序
+{
+	int d = maxbit(data, n);
+	int tmp[n];
+	int count[10]; //计数器
+	int i, j, k;
+	int radix = 1;
+	for (i = 1; i <= d; i++) //进行d次排序
+	{
+		for (j = 0; j < 10; j++)
+			count[j] = 0; //每次分配前清空计数器
+		for (j = 0; j < n; j++)
+		{
+			k = (data[j] / radix) % 10; //统计每个桶中的记录数
+			count[k]++;
+		}
+		for (j = 1; j < 10; j++)
+			count[j] = count[j - 1] + count[j]; //将tmp中的位置依次分配给每个桶
+		for (j = n - 1; j >= 0; j--) //将所有桶中记录依次收集到tmp中
+		{
+			k = (data[j] / radix) % 10;
+			tmp[count[k] - 1] = data[j];
+			count[k]--;
+		}
+		for (j = 0; j < n; j++) //将临时数组的内容复制到data中
+			data[j] = tmp[j];
+		radix = radix * 10;
 	}
 }
 
@@ -221,10 +326,22 @@ void TestSort()
 	//	cout << nums2[i] << " ";
 	//cout << endl;
 
-	cout << "堆排序：";
+	/*cout << "堆排序：";
 	HeapSort(nums);
 	for (auto e : nums)
 		cout << e << " ";
+	cout << endl;*/
+
+	//cout << "计数排序：";
+	//countSort(nums2, sizeof(nums2) / sizeof(nums2[0]));
+	//for (int i = 0; i < sizeof(nums2) / sizeof(nums2[0]); ++i)
+	//	cout << nums2[i] << " ";
+	//cout << endl;
+
+	cout << "桶排序：";
+	bucketSort(nums2, sizeof(nums2) / sizeof(nums2[0]));
+	for (int i = 0; i < sizeof(nums2) / sizeof(nums2[0]); ++i)
+		cout << nums2[i] << " ";
 	cout << endl;
 }
 
